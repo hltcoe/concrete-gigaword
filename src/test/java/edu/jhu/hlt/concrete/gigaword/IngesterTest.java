15d235dac50f1093aac5e74fac4534c209eaa4ba
Max Thomas <maxjthomas@gmail.com>
2015-03-19 16:32:14 -0400

    Add another test to cover another method.

--- src/test/java/edu/jhu/hlt/concrete/gigaword/IngesterTest.java
+++ src/test/java/edu/jhu/hlt/concrete/gigaword/IngesterTest.java
@@ -7,12 +7,16 @@ package edu.jhu.hlt.concrete.gigaword;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -34,12 +38,10 @@ import gigaword.interfaces.GigawordDocument;
 
 /**
  * @author max
- * 
+ *
  */
 public class IngesterTest {
 
-  private static final Logger logger = LoggerFactory.getLogger(IngesterTest.class);
-  
   Path p;
 
   /**
@@ -58,39 +60,49 @@ public class IngesterTest {
   @After
   public void tearDown() throws Exception {
   }
-  
+
   @Rule
   public TemporaryFolder tmpFolder = new TemporaryFolder();
 
   /**
    * Test method for {@link edu.jhu.hlt.gigaword.Ingester#convert(java.io.InputStream, edu.jhu.hlt.concrete.CommunicationType)}.
-   * 
-   * @throws IOException 
-   * @throws JsonMappingException 
-   * @throws JsonParseException 
-   * @throws ConcreteException 
+   *
+   * @throws IOException
+   * @throws JsonMappingException
+   * @throws JsonParseException
+   * @throws ConcreteException
    */
   @Test
-  public void concreteMapper() throws IOException, ConcreteException {
+  public void pathStringTest() throws IOException, ConcreteException {
     GigawordDocument pdc = new GigawordDocumentConverter().fromPathString(this.p.toString());
     Communication c = new ConcreteGigawordDocumentFactory().convert(pdc);
-    
+    this.testAgainstDogVsMan(c);
+  }
+
+  private void testAgainstDogVsMan(Communication c) throws ConcreteException {
     assertEquals("dog-bites-man_20141009.sgml", c.getId());
     assertEquals("other", c.getType().toLowerCase());
-    
+
     List<Section> sectionList = c.getSectionList();
     Section title = sectionList.get(0);
     assertEquals("Dog Bites Man\n", new SuperTextSpan(title.getTextSpan(), c).getText());
     assertEquals("Title", title.getKind());
 
-    for (Section s : sectionList)
-      logger.info(new SuperTextSpan(s.getTextSpan(), c).getText());
-    
     assertEquals(
         "John Smith, manager of ACME INC, was bit by a dog on March 10th, 2013.\n", new SuperTextSpan(sectionList.get(1).getTextSpan(), c).getText());
     assertEquals("Passage", sectionList.get(1).getKind());
-    
-    // new SuperCommunication(c).writeToFile(Files.createTempFile("target", "test-out.concrete").getFileName(), true);
+
     new SuperCommunication(c).writeToFile(tmpFolder.getRoot().toPath().resolve("test-out.concrete"), true);
+  }
+
+  @Test
+  public void sgmlStringTest() throws IOException, ConcreteException {
+    try (InputStream is = Files.newInputStream(p);
+        BufferedInputStream bis = new BufferedInputStream(is)) {
+      String sgml = IOUtils.toString(bis, StandardCharsets.UTF_8);
+      GigawordDocument pdc = new GigawordDocumentConverter().fromSGMLString(sgml);
+      Communication c = new ConcreteGigawordDocumentFactory().convert(pdc);
+      this.testAgainstDogVsMan(c);
+    }
   }
 }
